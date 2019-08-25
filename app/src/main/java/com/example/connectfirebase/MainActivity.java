7f3356c85addf4387e460c0d820b736a8407e364
SureshRawal21 <rxurace21@gmail.com>
2019-08-25 16:58:07 +0545

    upload image

--- app/src/main/java/com/example/connectfirebase/MainActivity.java
+++ app/src/main/java/com/example/connectfirebase/MainActivity.java
@@ -1,55 +1,110 @@
-package com.example.connectfirebase;
+ package com.example.connectfirebase;
 
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.example.connectfirebase.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 private Button btnButton;
-private EditText etText;
-DatabaseReference reff;
-Member member;
-long maxid = 0;
+private ImageView imageView;
+StorageReference mStorageReff;
+private StorageTask uploadTask;
+public Uri imguri;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mStorageReff= FirebaseStorage.getInstance().getReference("Images");
 
         btnButton = findViewById(R.id.btnButton);
-        etText = findViewById(R.id.etText);
-        member = new Member();
+        imageView = findViewById(R.id.ivImage);
 
-        reff = FirebaseDatabase.getInstance().getReference().child("Member");
-        reff.addValueEventListener(new ValueEventListener() {
+        imageView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if (dataSnapshot.exists())
-                    maxid = (dataSnapshot.getChildrenCount());
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
+            public void onClick(View v) {
+                Filechooser();
             }
         });
         btnButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String name = etText.getText().toString();
-                member.setName(name);
-                reff.child(String.valueOf(maxid+1)).setValue(member);
+
                 Toast.makeText(MainActivity.this,"data insert",Toast.LENGTH_LONG).show();
             }
         });
+    }
+
+    private void Filechooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent,1);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode ==1 && resultCode == RESULT_OK && data!=null && data.getData()!=null){
+            imguri=data.getData();
+            imageView.setImageURI(imguri);
+            if (uploadTask != null && uploadTask.isInProgress()){
+                Toast.makeText(MainActivity.this,"Upload in progress",Toast.LENGTH_LONG).show();
+            }else {
+               Fileuploader();
+            }
+
+        }
+    }
+    private String getExtension(Uri uri){
+        ContentResolver cr = getContentResolver();
+        MimeTypeMap mimeTypeMap =MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(cr.getType(uri));
+    }
+
+    private void Fileuploader() {
+        StorageReference Ref = mStorageReff.child(System.currentTimeMillis()+"."+getExtension(imguri));
+
+        uploadTask = Ref.putFile(imguri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // Get a URL to the uploaded content
+//                        Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                        Toast.makeText(MainActivity.this, "image uploaded successfully",Toast.LENGTH_LONG).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle unsuccessful uploads
+                        // ...
+                    }
+                });
     }
 }
